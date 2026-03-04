26308f927fc3b5776a19a4a3ae1e3312a7a0517d
srojgam0907 <srojgam0907@g.educaand.es>
2026-03-04 19:13:44 +0100

    Metodos para crear una carta de la clase Carta

--- src/blackJack/Carta.java
+++ src/blackJack/Carta.java
@@ -2,4 +2,41 @@ package blackJack;
 
 public class Carta {
 
+	private final TipoCarta valor;
+	private final Palo palo; 
+	
+	/**
+     * Constructor para crear una carta.
+     * @param palo El palo de la carta
+     * @param tipo El tipo o figura
+     */
+	public Carta(TipoCarta valor, Palo palo) {
+		this.valor= valor;
+		this.palo= palo;
+	}
+	
+	/**
+     * Obtiene el valor numérico base de la carta.
+     * @return El valor de la carta
+     */
+    public int getValor() {
+        return valor.getValorBase();  
+    }
+	
+    /**
+     * Comprueba si la carta es un AS.
+     * @return true si es un AS, false en caso contrario.
+     */
+    public boolean esAs() {
+        return valor == TipoCarta.AS; 
+    }
+	
+	/**
+     * Devuelve la representación visual de la carta.
+     * @return Formato legible, ejemplo: "AS♠" o "10♦".
+     */
+    @Override
+    public String toString() {
+        return valor + "" + palo.getSimbolo(); 
+    }
 }
