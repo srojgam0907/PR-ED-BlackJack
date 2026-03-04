3c03e1f256282cd88e48c3352656ae80fdc09d34
srojgam0907 <srojgam0907@g.educaand.es>
2026-03-04 18:48:41 +0100

    Valores del enum TipoCarta para el numero o figura

--- src/blackJack/TipoCarta.java
+++ src/blackJack/TipoCarta.java
@@ -2,4 +2,16 @@ package blackJack;
 
 public enum TipoCarta {
 
+	AS(11), DOS(2), TRES(3), CUATRO(4), CINCO(5), SEIS(6), SIETE(7), 
+	OCHO(8), NUEVE(9), DIEZ(10), JOTA(10), REINA(10), REY(10);
+	
+	private final int valor;
+
+    TipoCarta(int valor) {
+        this.valor = valor;
+    }
+
+    public int getValorBase() {
+        return valor; 
+    }
 }
