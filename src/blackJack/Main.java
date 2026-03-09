254fd73751a2b967570da9ebbfc20e75cb85906c
srojgam0907 <srojgam0907@g.educaand.es>
2026-03-09 19:54:48 +0100

    Clase main terminada

--- src/blackJack/Main.java
+++ src/blackJack/Main.java
@@ -3,7 +3,9 @@ package blackJack;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        GestorPartida partida = new GestorPartida();
+        
+        partida.iniciar();
 
 	}
 
