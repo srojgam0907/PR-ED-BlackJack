cbc5954150901a0ac5dfeb7691a67a84aca5f7f2
srojgam0907 <srojgam0907@g.educaand.es>
2026-03-09 17:42:09 +0100

    JavaDoc de la clase baraja

--- src/blackJack/Baraja.java
+++ src/blackJack/Baraja.java
@@ -8,11 +8,18 @@ public class Baraja {
 
 	private List<Carta> cartas;
 	
+	/**
+	 * Inicializa la baraja con las 52 cartas correspondientes.
+	 * También genera automáticamente las combinaciones de cartas.
+	 */
 	public Baraja() {
 		this.cartas= new ArrayList<>();
 		crearBaraja();
 	}
 	
+	/**
+	 * Llena la lista con las 52 cartas de la baraja estándar.
+	 */
 	private void crearBaraja() {
 		for(Palo palo : Palo.values()) {
 			for(TipoCarta valor : TipoCarta.values()) {
@@ -21,10 +28,17 @@ public class Baraja {
 		}
 	}
 	
+	/**
+	 * Mezcla las cartas de la lista de forma aleatoria
+	 */
 	public void barajar() {
 		Collections.shuffle(cartas);
 	}
 	
+	/**
+	 * Extrae de la baraja la carta de arriba para repartirla
+	 * @return la carta que se extrae
+	 */
 	public Carta repartir() {
 		if(cartas.isEmpty()) {
 			throw new IllegalStateException("No quedan más cartas");
