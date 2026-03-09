56a834f6fd19341e15afc0655bd3450b053a7d35
srojgam0907 <srojgam0907@g.educaand.es>
2026-03-09 19:25:40 +0100

    metodos de la clase del jugador

--- src/blackJack/Jugador.java
+++ src/blackJack/Jugador.java
@@ -1,5 +1,91 @@
 package blackJack;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Jugador {
 
+	private String nombre;
+	private List<Carta> jugada; 
+	private boolean plantado;
+
+	public Jugador(String nombre) {
+		this.nombre= nombre;
+		this.jugada= new ArrayList<>();
+		this.plantado= false;
+	}
+	
+	public void darCarta(Carta carta) {
+		jugada.add(carta);
+	}
+	
+	public void plantarse() {
+		plantado= true;
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+	
+	public boolean isPlantado() {
+		return plantado;
+	}
+	
+	public List<Carta> getJugada() {
+		return jugada; 
+	}
+	
+	public int calcularPuntuacion() {
+		int total= 0;
+		int contadorAS= 0;
+		
+		for(Carta carta : jugada) {
+			total += carta.getValor();
+			if(carta.esAs()) {
+				contadorAS++;
+			}
+		}
+		
+		while(total > 21 && contadorAS > 0) {
+			total-= 10;
+			contadorAS--;
+		}
+		
+		return total;
+	}
+	
+	public boolean pasado() {
+		if(calcularPuntuacion() > 21) {
+			return true;
+			
+		} else {
+			return false; 
+			
+		}
+	}
+	
+	@Override
+    public String toString() {
+        return nombre + ": " + jugada + " -> Puntos: " + calcularPuntuacion();
+    } 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
