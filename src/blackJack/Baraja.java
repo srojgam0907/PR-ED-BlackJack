0d1279f24278c8dbe0f9f680174f9ef6203ef1be
srojgam0907 <srojgam0907@g.educaand.es>
2026-03-09 17:34:29 +0100

    metodos para la baraja de la clase baraja

--- src/blackJack/Baraja.java
+++ src/blackJack/Baraja.java
@@ -1,5 +1,35 @@
 package blackJack;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Baraja {
 
+	private List<Carta> cartas;
+	
+	public Baraja() {
+		this.cartas= new ArrayList<>();
+		crearBaraja();
+	}
+	
+	private void crearBaraja() {
+		for(Palo palo : Palo.values()) {
+			for(TipoCarta valor : TipoCarta.values()) {
+				this.cartas.add(new Carta(valor, palo));
+			}
+		}
+	}
+	
+	public void barajar() {
+		Collections.shuffle(cartas);
+	}
+	
+	public Carta repartir() {
+		if(cartas.isEmpty()) {
+			throw new IllegalStateException("No quedan más cartas");
+		}
+		
+		return cartas.remove(cartas.size()-1);
+	}
 }
