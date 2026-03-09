db1a6df5aa14244552fdf454fc17854e54372ecc
srojgam0907 <srojgam0907@g.educaand.es>
2026-03-09 19:34:51 +0100

    JavaDoc de la clase jugador

--- src/blackJack/Jugador.java
+++ src/blackJack/Jugador.java
@@ -15,26 +15,48 @@ public class Jugador {
 		this.plantado= false;
 	}
 	
+	/**
+	 * Reparte una carta al jugador
+	 * @param carta una carta aleatoria de la baraja
+	 */
 	public void darCarta(Carta carta) {
 		jugada.add(carta);
 	}
 	
+	/**
+	 * Cambia el estado del jugador
+	 */
 	public void plantarse() {
 		plantado= true;
 	}
 	
+	/**
+	 * @return nombre El nombre del jugador
+	 */
 	public String getNombre() {
 		return nombre;
 	}
 	
+	/**
+	 * @return plantado El estado del jugador
+	 */
 	public boolean isPlantado() {
 		return plantado;
 	}
 	
+	/**
+	 * @return jugada La lista de cartas del jugador
+	 */
 	public List<Carta> getJugada() {
 		return jugada; 
 	}
 	
+	/**
+	 * Calcula la puntuación de las cartas del jugador
+	 * teniendo en cuenta los Ases. Si el jugador se pasa
+	 * de 21 y tiene algún AS, este pasa de valer 1 a valer 11
+	 * @return total La puntacion del jugador
+	 */
 	public int calcularPuntuacion() {
 		int total= 0;
 		int contadorAS= 0;
@@ -54,6 +76,10 @@ public class Jugador {
 		return total;
 	}
 	
+	/**
+	 * Comprueba si el jugador se ha pasado de 21
+	 * @return true si se ha pasado, false si no
+	 */
 	public boolean pasado() {
 		if(calcularPuntuacion() > 21) {
 			return true;
